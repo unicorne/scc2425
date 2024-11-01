17ab3e5f1dcf736292a5fc12234e6fa3cce5cec7
DimiJonas <dont@me>
2024-11-01 14:39:53 +0000

    added web-inf folder; added hibernate cfg

--- scc2425-tukano/src/main/java/tukano/impl/Token.java
+++ scc2425-tukano/src/main/java/tukano/impl/Token.java
@@ -9,7 +9,7 @@ public class Token {
 
 	private static final String DELIMITER = "-";
 	private static final long MAX_TOKEN_AGE = 300000;
-	private static String secret;
+	private static String secret = "summysecret";
 
 	public static void setSecret(String s) {
 		secret = s;
